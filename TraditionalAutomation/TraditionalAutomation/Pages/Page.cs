146cc648a3e8bd0cccde0b7f6c77d0d3a53edb60
anidanic <57825712+anidanic@users.noreply.github.com>
2019-11-16 10:32:56 +0200

    start
    created the main project for the traditional automation

---
+++ TraditionalAutomation/TraditionalAutomation/Pages/Page.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraditionalAutomation
+{
+    /// <summary>
+    /// start hour 10.30
+    /// </summary>
+    public class Page
+    {
+
+    }
+}
---
+++ TraditionalAutomation/TraditionalAutomation/TraditionalTestsCSharp/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraditionalAutomation.Tests
+{
+    /// <summary>
+    /// start hour 10.30
+    /// </summary>
+    [TestFixture]
+    public class BaseTest
+    {
+
+    }
+}
